1f1827162e2c986dbbcf9d21e765eefd646f0028
ssteja294 <ssteja294@gmail.com>
2024-09-05 00:29:16 +0530

    myDmart.py

--- myDmart.py
+++ myDmart.py
@@ -4,15 +4,15 @@ import mysql.connector
 
 DbConnection = mysql.connector.connect(host='138.68.140.83', user='saiteja', password='Saiteja@123', database='dbSaiteja')
 DbCursor = DbConnection.cursor()
-ItemTable = "Item"
+
 
 def LoadFields():
-  DbCursor.execute(f"SHOW COLUMNS FROM {ItemTable}")
+  DbCursor.execute("SHOW COLUMNS FROM Item")
   global Fields
   Fields = [FieldName[0] for FieldName in DbCursor.fetchall()]
 
 def PrintItems():
-  DbCursor.execute(f"SELECT * FROM {ItemTable}")
+  DbCursor.execute("SELECT * FROM Item")
   Items = DbCursor.fetchall()
   for Item in Items:
     for FieldCounter, Field in enumerate(Fields):
