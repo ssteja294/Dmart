adab63f2d63ad609c83df593991692db81501185
ssteja294 <ssteja294@gmail.com>
2024-09-05 10:16:54 +0530

    myDmart.py

--- myDmart.py
+++ myDmart.py
@@ -8,15 +8,15 @@ DbCursor = DbConnection.cursor()
 
 def loadItemFields():
   DbCursor.execute("SHOW COLUMNS FROM Item")
-  global Fields
-  Fields = [FieldName[0] for FieldName in DbCursor.fetchall()]
+  global itemFields
+  itemFields = [FieldName[0] for FieldName in DbCursor.fetchall()]
 
 def printItems():
   DbCursor.execute("SELECT * FROM Item")
   Items = DbCursor.fetchall()
   for Item in Items:
-    for FieldCounter, Field in enumerate(Fields):
-      print(f"{Field}: {Item[FieldCounter]}")
+    for itemFieldCounter, itemField in enumerate(itemFields):
+      print(f"{itemField}: {Item[itemFieldCounter]}")
     print("\n")
 
 loadItemFields()
